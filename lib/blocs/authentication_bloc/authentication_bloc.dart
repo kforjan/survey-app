1a05602b1caeb6659e235ae328ff8ba6aade6900
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-09-14 22:47:47 +0200

    add user repository

--- lib/blocs/authentication_bloc/authentication_bloc.dart
+++ lib/blocs/authentication_bloc/authentication_bloc.dart
@@ -6,13 +6,31 @@ import 'package:meta/meta.dart';
 part 'authentication_event.dart';
 part 'authentication_state.dart';
 
-class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
   AuthenticationBloc() : super(AuthenticationInitial());
 
   @override
   Stream<AuthenticationState> mapEventToState(
     AuthenticationEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is AuthenticationStarted) {
+      _mapAuthenticationStartedToState();
+    }
+    if (event is LoggedIn) {
+      _mapLoggedInToState();
+    }
+    if (event is LoggedOut) {
+      _mapALoggedOutToState();
+    }
+  }
+
+  Stream<AuthenticationState> _mapAuthenticationStartedToState() async* {}
+
+  Stream<AuthenticationState> _mapLoggedInToState() async* {}
+
+  Stream<AuthenticationState> _mapALoggedOutToState() async* {
+    yield AuthenticationFailure();
+    //TODO: signout func
   }
 }
---
+++ lib/data/user_repository.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserRepository {
+  UserRepository(this._firebaseAuth);
+
+  final FirebaseAuth _firebaseAuth;
+
+  Future<bool> get isSignedIn async {
+    final currentUser = _firebaseAuth.currentUser;
+
+    return currentUser != null;
+  }
+
+  Future<void> signInWithCredentials(String email, String password) {
+    return _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<UserCredential> signUp(String email, String password) async {
+    return await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() async {
+    return _firebaseAuth.signOut();
+  }
+
+  Future<User?> get currentUser async {
+    return _firebaseAuth.currentUser;
+  }
+}
