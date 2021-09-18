2f48194b8d3bd3502ce619642876f611df858d4a
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-09-18 16:31:30 +0200

    add create survey screen ui

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:survey_app/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:survey_app/blocs/survey_creation_bloc/survey_creation_bloc.dart';
 import 'package:survey_app/data/user_repository.dart';
 
 final locator = GetIt.instance;
@@ -9,4 +10,5 @@ void setup() {
   locator.registerFactory(() => AuthenticationBloc(locator()));
   locator.registerLazySingleton(() => UserRepository(locator()));
   locator.registerLazySingleton(() => FirebaseAuth.instance);
+  locator.registerLazySingleton(() => SurveyCreationBloc());
 }
--- lib/ui/create_survey/create_survey_screen.dart
+++ lib/ui/create_survey/create_survey_screen.dart
@@ -1,15 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:survey_app/blocs/survey_creation_bloc/survey_creation_bloc.dart';
+import 'package:survey_app/injection_container.dart' as di;
 
 class CreateSurveyScreen extends StatelessWidget {
   const CreateSurveyScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('create'),
+    return BlocProvider(
+      create: (context) => di.locator<SurveyCreationBloc>(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Create a survey'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: 4,
+                itemBuilder: (context, index) => _buildQuestionCard(context),
+              ),
+              _buildAddQustionCard(context)
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  Widget _buildQuestionCard(BuildContext context) {
+    return Card(
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAddQustionCard(BuildContext context) => Card(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.8,
+          height: 80,
+          margin: EdgeInsets.only(left: 40),
+          child: Center(
+            child: ListTile(
+              leading: Icon(
+                Icons.add,
+                color: Colors.black,
+              ),
+              title: Text('Add another question'),
+            ),
+          ),
+        ),
+      );
 }
